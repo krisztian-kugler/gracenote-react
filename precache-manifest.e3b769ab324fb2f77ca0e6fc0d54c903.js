725476059968e350a95f2b79d085ee22fc9e519a
Krisztian Kugler <kugler.krisztian@erise.hu>
2019-05-10 15:09:24 +0200

    Updates

---
+++ precache-manifest.e3b769ab324fb2f77ca0e6fc0d54c903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ab76cfb8fa9433c86b4ae1a21619a3",
+    "url": "/gracenote-react/index.html"
+  },
+  {
+    "revision": "93627c67f34fb97344ff",
+    "url": "/gracenote-react/static/css/main.0e364d53.chunk.css"
+  },
+  {
+    "revision": "eabdc1a00289f3e10d8f",
+    "url": "/gracenote-react/static/js/2.347142e1.chunk.js"
+  },
+  {
+    "revision": "93627c67f34fb97344ff",
+    "url": "/gracenote-react/static/js/main.5d19704a.chunk.js"
+  },
+  {
+    "revision": "b8d7b5e809647dd12ee9",
+    "url": "/gracenote-react/static/js/runtime~main.3cced928.js"
+  }
+]);
